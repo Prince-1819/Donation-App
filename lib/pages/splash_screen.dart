ba3b80b274f5fb0380af833327edffc2fa7dbf71
Prince Shah <shahprince2002@gmail.com>
2024-04-16 10:45:38 +0530

    init

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[400],
+      body: Center(
+        child: Container(
+          child: Image.asset('assets/images/splash.png'),
+          width: 375,
+          height: 237.55,
+        ),
+      ),
+    );
+  }
+}
